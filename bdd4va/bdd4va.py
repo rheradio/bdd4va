5e935aa9fc5c5e53fa36f4952cc3605088379080
Ruben Heradio <rheradio@issi.uned.es>
2022-05-30 19:50:18 +0200

    Bugs regarding small models fixed

--- bdd4va/bdd4va.py
+++ bdd4va/bdd4va.py
@@ -4,6 +4,8 @@ import subprocess, locale
 import re
 from pathlib import Path
 
+
+
 def set_global_constants():
     '''
     Private auxiliary function that configures the following global constants:
@@ -22,6 +24,7 @@ def set_global_constants():
         shell = subprocess.run(['pwd'], stdout=subprocess.PIPE)
     BDD4VAR_DIR = shell.stdout.decode(locale.getdefaultlocale()[1]).strip()
     os.chdir(caller_dir)
+
 set_global_constants()
 
 def run(binary, *args):
@@ -39,169 +42,211 @@ def run(binary, *args):
             command = [bin_file]
         else:
             command = [bin_file] + list(args)
-    return subprocess.run(command, stdout=subprocess.PIPE)
+    return subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.DEVNULL)
 
-def check_file_existence(filename, extension):
+
+def check_file_existence(filename, extension=None):
     '''
     Private auxiliary function that verifies if the input file exists    
     '''
-    if not os.path.isfile(filename):
+    if (not os.path.isfile(filename)) and (extension != None):
         filename = filename + '.' + extension
         if not os.path.isfile(filename):
             message = 'The file "' + filename + '" doesn\'t exist.'
             raise(Exception(message))
     return filename
 
-def bdd(model_file):
-    '''
-    Builds a BDD for a configuration model
-    :param model_file: A file specifying a configuration model with the SPLOT format (visit http://www.splot-research.org/)
-    :return: A dddmp file containing the BDD encoding of the model (dddmp is the format that the BDD library CUDD uses;
-    check https://github.com/vscosta/cudd). The dddmp file is generated in the same folder where model_file is located.
-    '''
-    # Check model_file
-    model_file = check_file_existence(model_file, "xml")
-    file_name = re.search('(.*)[.]xml', model_file).group(1)
 
-    # Run binary splot2logic
-    print("Preprocessing " + model_file + " to get its BDD...")
-    splot_process = run("splot2logic", "-use-XOR", model_file)
-    # Check that splot2logic was successful
-    try:
-        check_file_existence(file_name, "var")
-        check_file_existence(file_name, "exp")
-    except Exception:
-        message = 'BDD synthesis failed: the files "' + file_name + '.var" and "' + \
-                  file_name + '.exp" couldn\'t be generated.'
-        raise (Exception(message))
+class BDD:
 
-    # Run binary logic2bdd's execution
-    print("Synthesizing the BDD (this may take a while)...")
-    bdd_process = run("logic2bdd",
-                      "-line-length", "50",
-                      "-min-nodes", "100000",
-                      "-constraint-reorder", "minspan",
-                      "-base", file_name,
-                      file_name + ".var",
-                      file_name + ".exp")
-    # Check that logic2bdd's execution was successful
-    try:
-        check_file_existence(file_name, "dddmp")
-    except Exception:
-        message = 'BDD synthesis failed: the file "' + file_name + '.dddmp couldn\'t be generated.'
-        raise (Exception(message))
+    def __init__(self, model_file, synthesize_bdd=True):
+        '''
+        Builds a BDD for a configuration model
+        :param model_file: A file specifying a configuration model with the SPLOT format (visit http://www.splot-research.org/)
+        '''
 
-    # Remove auxiliary generated files
-    AUXILIARY_FILES = [".data", ".exp", ".reorder", ".tree", ".var"]
-    for f in AUXILIARY_FILES: Path(file_name + f).unlink()
+        if (not synthesize_bdd):
+            self.dddmp_file = check_file_existence(model_file, "dddmp")
+            return
 
-def sample(bdd_file, config_number, with_replacement=True):
-    '''
-    Generates a uniform random sample of size "config_number" from "bdd_file".
-    For detailed information, see the paper: R. Heradio, D. Fernandez-Amoros, J. Galindo,
-    D. Benavides, and D. Batory, "Uniform and Scalable Sampling of Highly Configurable Systems,” Submitted
-    to Empirical Software Engineering (currently under review), 2021.
-    :param bdd_file: dddmp file that stores a configuration model's BDD encoding (dddmp is the
-           format that the BDD library CUDD uses; check https://github.com/vscosta/cudd)
-    :param config_number: Number of configurations to be generated.
-    :param with_replacement: If it is True, every configuration is generated from scratch, independently
-           of the prior generated configurations. Accordingly, a configuration may be repeated in the sample. If
-           with_replacement is False then the sample is generated without replacement and there won't be any
-           repeated configurations (for a more detailed explanation, check https://en.wikipedia.org/wiki/Simple_random_sample).
-    :return: A list with the generated configurations. Each element in that list is a configuration, and
-             each configuration is in turn a list of strings encoding the feature values.
-    '''
-    # Check bdd_file
-    bdd_file = check_file_existence(bdd_file, "dddmp")
+        # Check model_file
+        model_file = check_file_existence(model_file, "xml")
+        file_name = re.search('(.*)[.]xml', model_file).group(1)
 
-    # Check config_number
-    if config_number <= 0:
-        message = 'config_number must be an integer greater than zero'
-        raise(Exception(message))
+        # Run binary splot2logic
+        print("Preprocessing " + model_file + " to get its BDD...")
+        splot_process = run("splot2logic", "-use-XOR", model_file)
+        # Check that splot2logic was successful
+        try:
+            check_file_existence(file_name, "var")
+            check_file_existence(file_name, "exp")
+        except Exception:
+            message = 'BDD synthesis failed: the files "' + file_name + '.var" and "' + \
+                      file_name + '.exp" couldn\'t be generated.'
+            raise (Exception(message))
 
-    # Run binary BDDSampler
-    parameters = ["-names"]
-    if not with_replacement:
-        parameters.append("-norep")
-    print("Getting a sample with", config_number, "configurations (this may take a while)...")
-    sample_process = run("BDDSampler", "-names", str(config_number), bdd_file)
-    result = sample_process.stdout.decode(locale.getdefaultlocale()[1])
-    line_iterator = iter(result.splitlines())
-    configurations = []
-    for line in line_iterator:
-        parsed_line = re.compile("\s+").split(line)
-        configuration = []
-        negation = ""
-        for element in parsed_line:
-            if element != "":
-                if element == "not":
-                    negation = "not "
+        # Run binary logic2bdd's execution
+        print("Synthesizing the BDD (this may take a while)...")
+        bdd_process = run("logic2bdd",
+                          "-line-length", "50",
+                          "-min-nodes", "100000",
+                          "-constraint-reorder", "minspan",
+                          "-base", file_name + ".dddmp",
+                          file_name + ".var",
+                          file_name + ".exp")
+        # Check that logic2bdd's execution was successful
+        try:
+            check_file_existence(file_name)
+        except Exception:
+            message = 'BDD synthesis failed: the file "' + file_name + '.dddmp couldn\'t be generated.'
+            raise (Exception(message))
+
+        # Remove auxiliary generated files
+        AUXILIARY_FILES = [".dddmp.data", ".exp", ".dddmp.reorder", ".tree", ".var", ".dddmp.applied"]
+        for f in AUXILIARY_FILES: Path(file_name + f).unlink()
+        self.dddmp_file = file_name
+
+    def sample(self, config_number, with_replacement=True):
+        '''
+        Generates a uniform random sample of size "config_number" from "bdd_file".
+        For detailed information, see the paper: R. Heradio, D. Fernandez-Amoros, J. Galindo,
+        D. Benavides, and D. Batory, "Uniform and Scalable Sampling of Highly Configurable Systems,” Submitted
+        to Empirical Software Engineering (currently under review), 2021.
+        :param bdd_file: dddmp file that stores a configuration model's BDD encoding (dddmp is the
+               format that the BDD library CUDD uses; check https://github.com/vscosta/cudd)
+        :param config_number: Number of configurations to be generated.
+        :param with_replacement: If it is True, every configuration is generated from scratch, independently
+               of the prior generated configurations. Accordingly, a configuration may be repeated in the sample. If
+               with_replacement is False then the sample is generated without replacement and there won't be any
+               repeated configurations (for a more detailed explanation, check https://en.wikipedia.org/wiki/Simple_random_sample).
+        :return: A list with the generated configurations. Each element in that list is a configuration, and
+                 each configuration is in turn a list of strings encoding the feature values.
+        '''
+        # Check bdd_file
+        bdd_file = check_file_existence(self.dddmp_file, "dddmp")
+
+        # Check config_number
+        if config_number <= 0:
+            message = 'config_number must be an integer greater than zero'
+            raise(Exception(message))
+
+        # Run binary BDDSampler
+        parameters = ["-names"]
+        if not with_replacement:
+            parameters.append("-norep")
+        print("Getting a sample with", config_number, "configurations (this may take a while)...")
+        sample_process = run("BDDSampler", "-names", str(config_number), bdd_file)
+        result = sample_process.stdout.decode(locale.getdefaultlocale()[1])
+        line_iterator = iter(result.splitlines())
+        configurations = []
+        for line in line_iterator:
+            parsed_line = re.compile("\s+").split(line)
+            configuration = []
+            negation = ""
+            for element in parsed_line:
+                if element != "":
+                    if element == "not":
+                        negation = "not "
+                    else:
+                        configuration.append(negation + element)
+                        negation = ""
+            configurations.append(configuration)
+        return configurations
+
+    def feature_probabilities(self):
+        '''
+        Computes the probability each model feature has to be included in a valid product.
+        That is, for every feature it returns the number of valid products with the feature activated
+        divided by the total number of valid products (a product is valid if it satisfies all model constraints).
+        For detailed information, see the paper: Heradio, R., Fernandez-Amoros, D., Mayr-Dorn, C., Egyed, A.:
+        Supporting the statistical analysis of variability models. In: 41st International Conference on Software
+        Engineering (ICSE), pp. 843–853. Montreal, Canada (2019).
+        :return: A dictionary with the format {feature_1: feature_1_probability, feature_2: feature_2_probability, ...}
+        '''
+        # Check bdd_file
+        bdd_file = check_file_existence(self.dddmp_file, "dddmp")
+
+        # Run binary feature_probabilities
+        print("Getting the feature probabilities (this may take a while)...")
+        feature_probabilities_process = run("feature_probabilities", bdd_file)
+        result = feature_probabilities_process.stdout.decode(locale.getdefaultlocale()[1])
+        line_iterator = iter(result.splitlines())
+        probabilities = {}
+        for line in line_iterator:
+            parsed_line = re.compile("\s+").split(line.strip())
+            probabilities[parsed_line[0]] = float(parsed_line[1])
+        return probabilities
+
+    def product_distribution(self):
+        '''
+        Computes the distribution of the number of activated features per product.
+        That is,
+            + How many products have 0 features activated?
+            + How many products have 1 feature activated?
+            + ...
+            + How many products have all features activated?
+        For detailed information, see the paper: Heradio, R., Fernandez-Amoros, D., Mayr-Dorn, C., Egyed, A.:
+        Supporting the statistical analysis of variability models. In: 41st International Conference on Software
+        Engineering (ICSE), pp. 843–853. Montreal, Canada (2019).
+        :return: A list that stores:
+            + In index 0, the number of products with 0 features activated.
+            + In index 1, the number of products with 1 feature activated.
+            ...
+            + In index n, the number of products with n features activated.
+        '''
+        # Check bdd_file
+        bdd_file = check_file_existence(self.dddmp_file, "dddmp")
+
+        # Run binary product_distribution
+        print("Getting the product distribution (this may take a while)...")
+        product_distribution_process = run("product_distribution", bdd_file)
+        result = product_distribution_process.stdout.decode(locale.getdefaultlocale()[1])
+        line_iterator = iter(result.splitlines())
+        distribution = []
+        for line in line_iterator:
+            parsed_line = re.compile("\s+").split(line.strip())
+            distribution.append(int(parsed_line[1]))
+        return distribution
+
+    def count(self, feature_assignment=[]):
+        '''
+        Computes the number of valid configurations.
+        :param feature_assignment: a list with a partial or a complete features' assignment
+               (e.g., ["f1", "not f3", "f5"])
+        :return: The number of valid configurations
+        '''
+        # Check bdd_file
+        bdd_file = check_file_existence(self.dddmp_file, "dddmp")
+
+        # Get all feature names
+        f = open(bdd_file, "r")
+        bdd_code = f.read()
+        varnames = re.search('varnames\\s+(.*)', bdd_code).group(1).split()
+        f.close()
+
+        expanded_assignment = []
+        for feature in feature_assignment:
+            ft = None
+            if re.match('not\\s+', feature):
+                ft = re.search('not\\s+(.*)', feature).group(1)
+                if varnames.count(ft) == 0:
+                    raise Exception(ft + " is not a valid feature of " + bdd_file)
                 else:
-                    configuration.append(negation + element)
-                    negation = ""
-        configurations.append(configuration)
-    return configurations
+                    ft += "=false"
+            else:
+                if varnames.count(feature) == 0:
+                    raise Exception(feature + " is not a valid feature of " + bdd_file)
+                else:
+                    ft = feature + "=true"
+            expanded_assignment.append(ft)
 
-def feature_probabilities(bdd_file):
-    '''
-    Computes the probability each model feature has to be included in a valid product.
-    That is, for every feature it returns the number of valid products with the feature activated
-    divided by the total number of valid products (a product is valid if it satisfies all model constraints).
-    For detailed information, see the paper: Heradio, R., Fernandez-Amoros, D., Mayr-Dorn, C., Egyed, A.:
-    Supporting the statistical analysis of variability models. In: 41st International Conference on Software
-    Engineering (ICSE), pp. 843–853. Montreal, Canada (2019).
-    :param bdd_file: dddmp file that stores a configuration model's BDD encoding (dddmp is the
-           format that the BDD library CUDD uses; check https://github.com/vscosta/cudd).
-    :return: A dictionary with the format {feature_1: feature_1_probability, feature_2: feature_2_probability, ...}
-    '''
-    # Check bdd_file
-    bdd_file = check_file_existence(bdd_file, "dddmp")
+        # Run counter
+        print("Counting the number of valid configurations (this may take a while)...")
+        count_process = run("counter", *expanded_assignment, bdd_file)
+        result = count_process.stdout.decode(locale.getdefaultlocale()[1])
+        return int(result)
 
-    # Run binary feature_probabilities
-    print("Getting the feature probabilities (this may take a while)...")
-    bdd_file = re.search('(.*)[.]dddmp', bdd_file).group(1)
-    feature_probabilities_process = run("feature_probabilities", bdd_file)
-    result = feature_probabilities_process.stdout.decode(locale.getdefaultlocale()[1])
-    line_iterator = iter(result.splitlines())
-    probabilities = {}
-    for line in line_iterator:
-        parsed_line = re.compile("\s+").split(line.strip())
-        probabilities[parsed_line[0]] = float(parsed_line[1])
-    return probabilities
 
-def product_distribution(bdd_file):
-    '''
-    Computes the distribution of the number of activated features per product.
-    That is,
-        + How many products have 0 features activated?
-        + How many products have 1 feature activated?
-        + ...
-        + How many products have all features activated?
-    For detailed information, see the paper: Heradio, R., Fernandez-Amoros, D., Mayr-Dorn, C., Egyed, A.:
-    Supporting the statistical analysis of variability models. In: 41st International Conference on Software
-    Engineering (ICSE), pp. 843–853. Montreal, Canada (2019).
-    :param bdd_file: dddmp file that stores a configuration model's BDD encoding (dddmp is the
-           format that the BDD library CUDD uses; check https://github.com/vscosta/cudd).
-    :return: A list that stores:
-        + In index 0, the number of products with 0 features activated.
-        + In index 1, the number of products with 1 feature activated.
-        ...
-        + In index n, the number of products with n features activated.
-    '''
-    # Check bdd_file
-    bdd_file = check_file_existence(bdd_file, "dddmp")
-
-    # Run binary product_distribution
-    print("Getting the product distribution (this may take a while)...")
-    bdd_file = re.search('(.*)[.]dddmp', bdd_file).group(1)
-    product_distribution_process = run("product_distribution", bdd_file)
-    result = product_distribution_process.stdout.decode(locale.getdefaultlocale()[1])
-    line_iterator = iter(result.splitlines())
-    distribution = []
-    for line in line_iterator:
-        parsed_line = re.compile("\s+").split(line.strip())
-        distribution.append(int(parsed_line[1]))
-    return distribution
 
 
 
--- test/test_bdd4va.py
+++ test/test_bdd4va.py
@@ -2,29 +2,33 @@ import unittest
 import glob
 import os
 from random import randint
+import re
 
 # Importing bdd4va from a relative path
 import sys
 sys.path.append("..")
-from bdd4va import sample, feature_probabilities, product_distribution, bdd
+from bdd4va import BDD
 
 class TestBdd4va(unittest.TestCase):
 
     def test_bdd(self):
         try:
-            bdd("model_examples/DellSPLOT/DellSPLOT.xml")
-            bdd("model_examples/LargeAutomotive/LargeAutomotive.xml")
+            my_bdd = BDD("model_examples/pizzas/pizzas.xml")
+            my_bdd = BDD("model_examples/Truck/Truck.xml")
+            my_bdd = BDD("model_examples/mobilemedia2/mobilemedia2.xml")
+            my_bdd = BDD("model_examples/tankwar/tankwar.xml")
+            my_bdd = BDD("model_examples/DellSPLOT/DellSPLOT.xml")
         except ExceptionType:
-            self.fail("bdd() raised an Exception.")
-
+            self.fail("my_bdd() raised an Exception.")
 
     def test_sample(self):
         models = glob.glob("model_examples/*/*.dddmp", recursive=True)
         for model in models:
             print("Testing function sample with " + model)
             model = model.replace("\\", "/")
+            my_bdd = BDD(model, False)
             config_number = randint(1, 10)
-            configurations = sample(model, config_number)
+            configurations = my_bdd.sample(config_number)
             self.assertEqual(len(configurations), config_number)
 
     def test_feature_probabilities_and_product_distribution(self):
@@ -32,12 +36,20 @@ class TestBdd4va(unittest.TestCase):
         for model in models:
             print("Testing functions feature_probabilities and product_distribution with " + model)
             model = model.replace("\\", "/")
-            probabilities = feature_probabilities(model)
-            distribution = product_distribution(model)
+            my_bdd = BDD(model, False)
+            probabilities = my_bdd.feature_probabilities()
+            distribution = my_bdd.product_distribution()
             # Distribution has one element more than probabilities:
             # the number of products with zero features activated
             self.assertEqual(len(probabilities), len(distribution) - 1)
 
+    def test_count(self):
+        models = glob.glob("model_examples/*/*.dddmp", recursive=True)
+        for model in models:
+            print("Testing function count with " + model)
+            model = model.replace("\\", "/")
+            my_bdd = BDD(model, False)
+            confs = my_bdd.count()
 
 if __name__ == '__main__':
     unittest.main()
